46d1581ebdd708bdb65a14a6efd93c7e18056305
SeiCp <seycuartero@gmail.com>
2025-05-14 21:36:12 +0200

    first steps

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
+const listaUl = document.querySelector(".js-listado"); 
 console.log('>> Ready :)');
+
+let url = "https://raw.githubusercontent.com/Adalab/resources/master/apis/products.json";
+
+fetch (url)
+.then(response =>response.json())
+.then(products => {
+    console.log (products)
+    
+    for (let product of products) {
+        listaUl.innerHTML += `<li class="product-card">
+        <p> ${product.title}</p>
+        </li> `; 
+    }
+
+})
