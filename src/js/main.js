049e67f1ccd405d7225ebce72988a55029b69f35
SeiCp <seycuartero@gmail.com>
2025-05-15 13:51:25 +0200

    changes shopping card

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,18 @@
 const listaUl = document.querySelector(".js-listado"); 
 console.log('>> Ready :)');
 
+const product = {
+    "id": 1,
+    "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    "price": 109.95,
+    "category": "men's clothing",
+    "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    "rating": {
+    "rate": 3.9,
+    "count": 120
+    }
+};
+
 let url = "https://raw.githubusercontent.com/Adalab/resources/master/apis/products.json";
 
 fetch (url)
@@ -11,9 +23,11 @@ fetch (url)
     console.log (products)
     
     for (let product of products) {
-        listaUl.innerHTML += `<li class="product-card">
+        listaUl.innerHTML += `<li class="product_card">
+        <img src= ${product.image} />
         <p> ${product.title}</p>
-        </li> `; 
+        <p> ${product.price}</p>
+        </li>`; 
     }
 
 })
