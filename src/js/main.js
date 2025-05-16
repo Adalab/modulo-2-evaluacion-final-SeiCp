13e6df54deffd4d9c53a9692f02c364451986e78
SeiCp <seycuartero@gmail.com>
2025-05-16 13:51:28 +0200

    filter

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,59 @@
 'use strict';
 
 const listaUl = document.querySelector(".js-listado"); 
+const input = document.querySelector(".js-input")
+const btnFind = document.querySelector(".js-btn-form")
+
 console.log('>> Ready :)');
 
-const product = {
-    "id": 1,
-    "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-    "price": 109.95,
-    "category": "men's clothing",
-    "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-    "rating": {
-    "rate": 3.9,
-    "count": 120
-    }
-};
+
 
 let url = "https://raw.githubusercontent.com/Adalab/resources/master/apis/products.json";
 
+//funciones (del infierno)
+function renderProductos (listaProductos) {
+    listaUl.innerHTML = "";
+
+        for (let product of listaProductos) {
+            let imageURL;
+    
+            if (!product.image){
+                imageURL = "https://placehold.co/600x400";
+            }else {
+                imageURL = product.image;
+            }    
+        
+            listaUl.innerHTML += `<li class="product-card">
+            <img src= "${product.image}" alt= ${product.title}/>
+            <p> ${product.title}</p>
+            <p> ${product.price}</p>
+            </li>`; 
+        }    
+        
+}
+
+let allProducts = []; //variable global de todos los productos
+
+function handleClickFind(event){
+    event.preventDefault();
+
+    let valueFind = input.value;
+
+    const productsFiltrados = allProducts.filter(productItem => productItem.title.toLowerCase().includes(valueFind.toLowerCase()));
+    console.log(productsFiltrados); // 👈 esto sí funciona
+    renderProductos(productsFiltrados);
+
+}
+
+btnFind.addEventListener("click", handleClickFind);
+
+
+
+//carga los datos de la API
 fetch (url)
 .then(response =>response.json())
 .then(products => {
-    console.log (products)
-    
-    for (let product of products) {
-        let imageURL;
-
-        if (!product.image){
-            imageURL = "https://placehold.co/600x400";
-        }else {
-            imageURL = product.image;
-        }
-
-        listaUl.innerHTML += `<li class="product-card">
-        <img src= "${product.image}" alt= ${product.title}/>
-        <p> ${product.title}</p>
-        <p> ${product.price}</p>
-        </li>`; 
-    }
-
+    console.log(products)
+    allProducts = products;
+    renderProductos(products)
 })
